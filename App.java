05018c7dceffbe9dda2c4624bf05bb4e9095ec8c
Krolr0609 <nick.arty@gmail.com>
2016-10-07 13:37:23 +0300

    errors fix

--- App.java
+++ App.java
@@ -8,35 +8,34 @@ import static java.lang.Math.*;
 
 public class App {
 
+	public static String separator = ";"; //Символ, выбранный в кач-ве разделителя
+	public static String filepath_in = "note.txt";
+	public static String filepath_out = "out.txt";
+
 	public static void main(String[] args) throws IOException {
 		Scanner s = null;
 		PrintWriter output = null;
-		String separator = ";"; //Символ, выбранный в кач-ве разделителя
 		String part = " ";
 		int x;
 		int y;
 		try{
-			s = new Scanner(new BufferedReader(new FileReader("note.txt")));
-			output = new PrintWriter(new FileWriter("out.txt"));
-            while (s.hasNext()) {
-            	if (s.hasNext()){
-            		part = s.next();
-            		x = Integer.parseInt(part.split(separator)[0]);
-            		y = Integer.parseInt(part.split(separator)[1]);
+			s = new Scanner(new BufferedReader(new FileReader(filepath_in)));
+			output = new PrintWriter(new FileWriter(filepath_out));
+			while (s.hasNext()) {
+				part = s.next();
+				x = Integer.parseInt(part.split(separator)[0]);
+				y = Integer.parseInt(part.split(separator)[1]);
             		//System.out.println("x = "+x+" y = "+y); вывод x,y для проверки
-            		System.out.println("3 * "+x+"^3 + Pi * "+y+"^2"+" = "+pow(x,3)+PI*sqrt(y));
-            		output.println("3 * "+x+"^3 + Pi * "+y+"^2"+" = "+pow(x,3)+PI*sqrt(y));
-            	}else{
-            		s.next();
-            	}
-            }
+				System.out.println("3 * "+x+"^3 + Pi * "+y+"^2"+" = "+pow(x,3)+PI*pow(y,2));
+				output.println("3 * "+x+"^3 + Pi * "+y+"^2"+" = "+pow(x,3)+PI*pow(y,2));
+			}
 		}finally{
 			if (s != null) {
 				s.close();
 			}
 			if (output != null) {
-                output.close();
-            }
+				output.close();
+			}
 		}
 	}
 }
